38a8f604f1ee0fbae279912fad2755a3c7ad2d85
AbrarKivande <abrar30752@gmail.com>
2025-03-03 18:38:37 +0530

    5 and 6 lecture added

---
+++ Lecture 5/PrimitiveDatatype.java
@@ -0,0 +1,21 @@
+public class PrimitiveDatatype {
+    public static void main(String[] args) {
+        int i = 1234567890;
+        System.out.println("Here is my Integer Value:- "+i);
+
+        long l = 1234567890123412334L;
+        System.out.println("Here is my Long Value:- "+l);
+
+        float f = 2.0202020202020202002020202020f;
+        System.out.println("Here is my Float Value:- "+f);
+
+        Double d = 2.0202020202020202002020202020;
+        System.out.println("Here is my Double Value:- "+d);
+
+        char c = 'A';
+        System.out.println("Here is my Character Value:- "+c);
+
+        Boolean b = false;
+        System.out.println("Here is my Boolean Value:- "+b);
+    }
+}
