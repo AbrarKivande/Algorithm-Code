523de5719f61c0276b39125b2f82a6bd5c768f8c
AbrarKivande <abrar30752@gmail.com>
2025-04-03 23:45:40 +0530

    Lecture No 37 added

---
+++ Lecture 37/String_Rev.java
@@ -0,0 +1,12 @@
+public class String_Rev {
+    public static void main(String[] args) {
+        String s = "abrar";
+
+        String e = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            e = e + s.charAt(i);
+        }
+        System.out.println("here is reverse of string: "+e);
+    }
+}
